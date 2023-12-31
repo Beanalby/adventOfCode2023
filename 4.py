63331228feb96f559148df5e36a7193d148ec950
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2023-12-31 18:14:41 -0500

    First function of 4.py

---
+++ 4.py
@@ -0,0 +1,20 @@
+import unittest
+
+def getSuccessfulNumbers(gameText):
+    (winningText, playerText) = gameText.split(" | ")
+    winningNums = filter(lambda x: int(x), winningText.split())
+    playerNums = filter(lambda x: int(x), playerText.split())
+    return set(winningNums) & set(playerNums)
+
+class TestGuts(unittest.TestCase):
+    def test_getSuccessfulNumbers(self):
+        self.assertEqual(getSuccessfulNumbers("41 48 83 86 17 | 83 86  6 31 17  9 48 53"), set(["48", "83", "17", "86"]))
+        self.assertEqual(getSuccessfulNumbers("13 32 20 16 61 | 61 30 68 82 17 32 24 19"), set(["32", "61"]))
+        self.assertEqual(getSuccessfulNumbers(" 1 21 53 59 44 | 69 82 63 72 16 21 14  1"), set(["1", "21"]))
+        self.assertEqual(getSuccessfulNumbers("41 92 73 84 69 | 59 84 76 51 58  5 54 83"), set(["84"]))
+        self.assertEqual(getSuccessfulNumbers("87 83 26 28 32 | 88 30 70 12 93 22 82 36"), set())
+        self.assertEqual(getSuccessfulNumbers("31 18 13 56 72 | 74 77 10 23 35 67 36 11"), set())
+
+if __name__ == "__main__":
+    unittest.main()
+    # print("MAIN.");
