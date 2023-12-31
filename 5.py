3fdf9328c1a72bf20ee57d2231d189b45cf3f524
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2023-12-31 18:14:41 -0500

    5p1 Stage implemented, onto Game!

--- 5.py
+++ 5.py
@@ -13,33 +13,57 @@ class Mapper:
         self.sourceStart=sourceStart
         self.rangeLength=rangeLength
 
-    def doesMapperApply(self, num):
+    def isCorrectMapper(self, num):
         return num >= self.sourceStart \
             and num < self.sourceStart+self.rangeLength
 
     def apply(self, num):
-        if not self.doesMapperApply(num):
+        if not self.isCorrectMapper(num):
             error = "num {} shouldn't be used in mapper {}-{}"\
                 .format(num, self.sourceStart, self.sourceStart+self.rangeLength)
             raise ValueError(error)
         return num + (self.destStart-self.sourceStart)
 
+class Stage:
+    def __init__(self):
+        self.mappers = []
+
+    def addMapper(self, mapper):
+        self.mappers.append(mapper)
+
+    def apply(self, num):
+        for m in self.mappers:
+            if m.isCorrectMapper(num):
+                return m.apply(num)
+        # no mapper were right, leave as-is
+        return num
+
 class TestGuts(unittest.TestCase):
     def test_Mapper(self):
         m = Mapper(50, 98, 2)
-        self.assertEqual(m.doesMapperApply(-1), False)
-        self.assertEqual(m.doesMapperApply(0), False)
-        self.assertEqual(m.doesMapperApply(1), False)
-        self.assertEqual(m.doesMapperApply(49), False)
-        self.assertEqual(m.doesMapperApply(50), False)
-        self.assertEqual(m.doesMapperApply(51), False)
-        self.assertEqual(m.doesMapperApply(97), False)
-        self.assertEqual(m.doesMapperApply(98), True)
-        self.assertEqual(m.doesMapperApply(99), True)
-        self.assertEqual(m.doesMapperApply(100), False)
+        self.assertEqual(m.isCorrectMapper(-1), False)
+        self.assertEqual(m.isCorrectMapper(0), False)
+        self.assertEqual(m.isCorrectMapper(1), False)
+        self.assertEqual(m.isCorrectMapper(49), False)
+        self.assertEqual(m.isCorrectMapper(50), False)
+        self.assertEqual(m.isCorrectMapper(51), False)
+        self.assertEqual(m.isCorrectMapper(97), False)
+        self.assertEqual(m.isCorrectMapper(98), True)
+        self.assertEqual(m.isCorrectMapper(99), True)
+        self.assertEqual(m.isCorrectMapper(100), False)
 
         self.assertEqual(m.apply(98), 50)
         self.assertEqual(m.apply(99), 51)
+
+    def test_Stage(self):
+        s = Stage()
+        s.addMapper(Mapper(50, 98, 2))
+        s.addMapper(Mapper(52, 50, 48))
+        testCases = [ [0, 0], [1, 1], [48, 48], [49, 49], [50, 52], [51, 53], \
+                     [52, 54], [96, 98], [97, 99], [98, 50], [99, 51], [100, 100]]
+        for testRun in testCases:
+            (testInput, testOutput) = testRun
+            self.assertEqual(s.apply(testInput), testOutput)
 
 if __name__=="__main__":
     unittest.main()
