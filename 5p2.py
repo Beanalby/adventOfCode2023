02cc1333fcb75f30f48c4d1a351a335c8e3c5983
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2023-12-31 18:14:42 -0500

    5p2: Stage added, ready to put them together into a game

--- 5p2.py
+++ 5p2.py
@@ -9,7 +9,6 @@ from datetime import datetime
 # 39 0 15
 
 def normalizeAnswerSet(answerSet):
-    # breakpoint()
     answerSorted = sorted(answerSet, key=lambda x: x[0])
     answerCurrent = None
     answersNew = []
@@ -17,7 +16,7 @@ def normalizeAnswerSet(answerSet):
         if not answerCurrent:
             answerCurrent = answer
             continue
-        if answerCurrent[1] > answer[0]:
+        if answerCurrent[1] >= answer[0]:
             # answer gets consumed into answerCurrent
             # expand answer's maxRange if necessary
             newStart = min(answer[0],answerCurrent[0])
@@ -65,7 +64,7 @@ class Mapper:
         transformedEnd = min(answer[1],self.sourceEnd)+offset
         if(transformedStart < transformedEnd):
             answerTransformed = (transformedStart, transformedEnd)
-        
+
         # extract the above section
         if answer[1] > self.sourceEnd:
             answerAbove = (max(answer[0], self.sourceEnd), answer[1])
@@ -76,18 +75,60 @@ class Mapper:
 
         return (answerBelow, answerTransformed, answerAbove)
 
+class Stage:
+    def __init__(self, name):
+        self.name = name
+        self.mappers = []
+    def __str__(self):
+        s = self.name + ":";
+        for m in self.mappers:
+            s += "\n" + str(m)
+        return s
+
+    def addMapper(self, mapper):
+        self.mappers.append(mapper)
+        self.mappers.sort()
+
+    def apply(self, answerSet):
+        answersForOtherMappers = []
+        answerOutput = []
+        # run the answer set through each of the mappers
+        for mapper in self.mappers:
+            # run each of the answers through this mapper
+            # anything transformed goes to the output,
+            # anything before/after is still fother for other mappers
+            for answer in answerSet:
+                (answerBefore, answerTransformed, answerAfter) = mapper.splitAnswer(answer)
+                if answerBefore:
+                    answersForOtherMappers.append(answerBefore)
+                if answerAfter:
+                    answersForOtherMappers.append(answerAfter)
+                if answerTransformed:
+                    answerOutput.append(answerTransformed)
+
+            # this mapper's done, take everything this mapper didn't
+            # use and prep it for the other mappers
+            answerSet = normalizeAnswerSet(answersForOtherMappers)
+            answersForOtherMappers=[]
+
+        # anything left in answerSet when we're done with all mappers
+        # gets passed through as-is
+        return normalizeAnswerSet(answerOutput + answerSet)
+
 class TestGuts(unittest.TestCase):
 
     def test_normalizeAnswers(self):
         self.assertEqual(normalizeAnswerSet([(93, 94), (58, 111), (99, 182), (60, 125)]), [(58, 182)])
         self.assertEqual(normalizeAnswerSet([(79, 93), (55, 68)]), [(55, 68), (79, 93)])
         self.assertEqual(normalizeAnswerSet([(29, 53), (86, 168), (41, 44), (21, 37)]), [(21, 53), (86, 168)])
+        self.assertEqual(normalizeAnswerSet([(1, 50), (50, 52), (52, 100)]), [(1, 100)])
 
     def test_Mapper(self):
         m = Mapper(50, 98, 100)
         self.assertEqual(str(m), "98->100 to 50->52")
         # self.assertEqual(m.apply(98), 50)
         # self.assertEqual(m.apply(99), 51)
+
     def test_MapperSplit(self):
         # test the pen & paper version
         m = Mapper(50, 5, 10)
@@ -102,6 +143,14 @@ class TestGuts(unittest.TestCase):
         m2 = Mapper(52, 50, 98)
         self.assertEqual(m1.splitAnswer((79, 93)), ((79,93), None, None))
         self.assertEqual(m2.splitAnswer((79, 93)), (None, (81, 95), None))
+
+    def test_Stage(self):
+        s = Stage("seed-to-soil")
+        s.addMapper(Mapper(50, 98, 100))
+        s.addMapper(Mapper(52, 50, 98))
+        self.assertEqual(s.apply([(79, 93)]), [(81, 95)])
+        self.assertEqual(s.apply([(79, 93),(55,68)]), [(57,70),(81, 95)])
+        self.assertEqual(s.apply([(1, 98)]), [(1,50),(52,100)])
 
 if __name__=="__main__":
     # with open("5.txt") as inputFile:
