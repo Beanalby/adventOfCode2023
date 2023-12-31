e53ef7e836fdda127991628bcdc932d8b6bdbf07
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2023-12-31 18:14:41 -0500

    implemented applyGames, 4p2 now done

--- 4p2.py
+++ 4p2.py
@@ -5,7 +5,7 @@ import unittest
 class MatchGame:
     def __init__(self, cardsText):
         self.gameLines = cardsText.split("\n")
-        self.gameCopies = defaultdict(lambda: 1)
+        self.gameCopies = defaultdict(lambda: int(1))
 
     def getSuccessfulNumbers(self, gameText):
         (winningText, playerText) = gameText.split(" | ")
@@ -14,16 +14,13 @@ class MatchGame:
         return set(winningNums) & set(playerNums)
 
     def getGameNumber(self, line):
-        (cardAndNum, gameText) = line.split(": ")[0]
-        return cardAndNum.split()[1]
+        (cardAndNum, gameText) = line.split(": ")
+        return int(cardAndNum.split()[1])
 
     def getGameSuccesses(self, line):
         (cardAndNum, gameText) = line.split(": ")
         numSet = self.getSuccessfulNumbers(gameText)
-        if len(numSet) == 0:
-            return 0
-        else:
-            return math.pow(2, len(numSet)-1)
+        return len(numSet)
     
     def getCopyCount(self, gameNumber):
         return self.gameCopies[gameNumber]
@@ -36,11 +33,17 @@ class MatchGame:
         for i in range(1, successfulNumber+1):
             self.incrementCopies(gameNumber+i, incrementNumber)
 
-    def applyGames():
-        for i in self.gameLines:
+    def applyGames(self):
+        self.gameCopies = defaultdict(lambda: 1)
+        for line in self.gameLines:
+            if not line:
+                continue
             gameNumber = self.getGameNumber(line)
-            gameSuccesses = getGameSuccesses(line)
-        
+            gameSuccesses = self.getGameSuccesses(line)
+            self.incrementCopiesFromSuccess(gameNumber, gameSuccesses)
+            # print("After {}, now copies={}".format(gameNumber, self.gameCopies))
+        # print("+++ done, copies={}".format(self.gameCopies))
+        return sum(self.gameCopies.values())
 
 gameSample = MatchGame(open("4.txt").read())
 gameReal = MatchGame(open("4real.txt").read())
@@ -55,7 +58,7 @@ class TestGuts(unittest.TestCase):
         self.assertEqual(gameSample.getSuccessfulNumbers("31 18 13 56 72 | 74 77 10 23 35 67 36 11"), set())
 
     def test_getGameSuccesses(self):
-        self.assertEqual(gameSample.getGameSuccesses("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"), 8)
+        self.assertEqual(gameSample.getGameSuccesses("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"), 4)
         self.assertEqual(gameSample.getGameSuccesses("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"), 2)
         self.assertEqual(gameSample.getGameSuccesses("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"), 2)
         self.assertEqual(gameSample.getGameSuccesses("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"), 1)
@@ -77,9 +80,13 @@ class TestGuts(unittest.TestCase):
         gameSample.incrementCopiesFromSuccess(6, 0)
         self.assertEqual(gameSample.gameCopies, {1: 1, 2: 2, 3: 4, 4: 8, 5: 14, 6: 1})
         
+    def test_applyGames(self):
+        self.assertEqual(gameSample.applyGames(), 30)
+
 if __name__ == "__main__":
-    # print("real={}".format(sumGames(gameReal)))
-    unittest.main()
+    print("real={}".format(gameReal.applyGames()))
+    # print("sample={}".format(gameSample.applyGames()))
+    # unittest.main()
     # print("start: {}".format(gameSample.gameCopies))
     # gameSample.incrementCopiesFromSuccess(1, 4)
     # print("after 1: {}".format(gameSample.gameCopies))
